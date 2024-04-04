e490c1a6e58aad717a5f15ce6f2b1fc24eb8cfe8
kelrysro <kelrysilvarodrigues@gmail.com>
2024-04-04 08:42:46 -0300

    atualizações nas models ACESSORIO e categoria  e criação das models MARCA e COR

--- app/urls.py
+++ app/urls.py
@@ -9,13 +9,19 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet
+from core.views import UserViewSet 
+from core.views import  AcessorioViewSet
+from core.views import CategoriaViewSet
+from core.views import CorViewSet
+from core.views import MarcaViewSet
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
-router.register(r"acessorios", AcessorioViewSet, basename="acessorios")
-router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"cores", CorViewSet)
+router.register(r"marcas", MarcaViewSet,) 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
